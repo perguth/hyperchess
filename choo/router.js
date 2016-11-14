8fd2c1ece14c78ddbaa52793df6f5df5ac4184c5
Per Guth <mail@perguth.de>
2016-11-14 21:13:32 +0100

    empty

--- choo/router.js
+++
@@ -1,8 +0,0 @@
-module.exports = core => route => [
-  route('/', require('../views/dashboard')(core)),
-  route('/404', require('../views/error')(core)),
-  route('/chessboard', require('../views/chessboard')(core)),
-  route('/join', [
-    route('/:connectionId', require('../views/chessboard')(core))
-  ])
-]
--- pieces/LICENSE.md
+++
@@ -1,1 +0,0 @@
-All chess pieces by [Colin M.L. Burnett](https://en.wikipedia.org/wiki/User:Cburnett) licensed under [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/legalcode).
